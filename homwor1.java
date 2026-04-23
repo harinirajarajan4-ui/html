c82116c733ac274d7b6c28866514ed1b112941df
harinirajarajan4-ui <harinirajarajan4@gmail.com>
2026-04-23 23:35:18 +0530

    Update homwor1.java
    printef output

--- homwor1.java
+++ homwor1.java
@@ -4,6 +4,9 @@ class shop{
         int quantity=4;
         double unitprice=18.75;
         double totalprice=quantity*unitprice;
+         System.out.println("itemname: " + itemname);
+        System.out.println("quantity: " + quantity);
+        System.out.println("unit price: " + unitprice);
         System.out.println("total price: " + totalprice);
     }
 }
